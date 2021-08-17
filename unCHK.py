c6d55b49f566f5b22a8398c228172eb9a05fe146
Néstor Delgado <ndelgado@corrosioncic.com>
2021-08-17 23:03:36 +0000

    new version

--- unCHK.py
+++ unCHK.py
@@ -3,9 +3,10 @@ import glob
 import os
 
 path =  os.path.dirname(__file__)
-new_folder = path + "/recovery/Recuperado"
-print("hola")
+new_folder = path + "/recovery"
 
+if not os.path.exists(new_folder):
+       os.mkdir(new_folder)
 
 def chk(file):
     kind = filetype.guess(file)
@@ -15,17 +16,20 @@ def chk(file):
         return
 
 
-    print('File : %s' % file)
     print('File extension: %s' % kind.extension)
     print('File MIME type: %s' % kind.mime)
-    print()
-    print()
-    print()
-    # print( file.split(".msox.zip")[0])
-    # new_name = file.split(".CHK")[0]+"."+kind.extension
+    print( file.split(".CHK")[0])
+    new_name = file.split(".CHK")[0]+"."+kind.extension
     # os.rename(file, file.split(".CHK")[0]+"."+kind.extension)
+    os.rename(file, os.path.join(new_folder, new_name))
 
-for path,dirs,files in os.walk(new_folder):
-    for file in files:
-        chk(f'{path}/{file}')
-        # print(f'{path}/{file}')
+
+cwd = os.getcwd()
+mylist = [os.path.join(cwd, f) for f in os.listdir(cwd) if 
+os.path.isfile(os.path.join(cwd, f))]
+# print(mylist) 
+
+
+# mylist = [f for f in glob.glob("*.CHK")]
+for file in mylist:
+   print(chk(file))
